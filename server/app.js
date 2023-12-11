f2c92381750fed90c0cf2ebdb4926c79b37dfd00
Looming <51847400+VladislavHryhoriev@users.noreply.github.com>
2023-12-11 10:08:29 +0000

    update

--- server/app.js
+++ server/app.js
@@ -3,22 +3,23 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer((req, res) => {
+
 	let variableValues = {
 		title: 'Заголовок',
 		content: 'Текст статьи'
 	};
 
-	if (req.url === '/user') {
-		fs.readFile("./server/index.html", 'utf-8', (error, data) => {
-			let text = "1"
+	// if (req.url === '/user') {
+	// 	fs.readFile("./server/index.html", 'utf-8', (error, data) => {
+	// 		let text = ""
 
-			let newData = data.replace(/{(.+)}/gi, (match, variableName) => {
-				console.log(`${match}, ${variableName}`);
-			});
+	// 		let newData = data.replace(/{(.+)}/gi, (match, variableName) => {
+	// 			console.log(`${match}, ${variableName}`);
+	// 		});
 
-			res.setHeader('Content-Type', 'text/html; charset=utf-8')
-			res.end(data.replace(/{.+}/g, text));
-		});
-	}
+	// 		res.setHeader('Content-Type', 'text/html; charset=utf-8')
+	// 		res.end(1);
+	// 	});
+	// }
 
-}).listen(3000);
+}).listen(5050);
